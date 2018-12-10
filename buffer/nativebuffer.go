a061f7811cf96ebe384803bd207417f82ff8a9f6
yhhaiua <471537867@qq.com>
2018-12-10 17:10:10 +0800

    buffer
    buffer

--- buffer/nativebuffer.go
+++ buffer/nativebuffer.go
@@ -14,11 +14,18 @@ func (buf *NativeBuffer)ReadNString() string  {
 	length := buf.ReadNInt16()
 	return  string(buf.Next(length))
 }
-//ReadNInt16 读取int16
+//ReadNInt16 读取int16 返回int
 func (buf *NativeBuffer)ReadNInt16() int {
 	var v int16
 	binary.Read(buf, binary.BigEndian,&v)
 	return int(v)
+}
+
+//ReadNShort 读取int16
+func (buf *NativeBuffer)ReadNShort() int16 {
+	var v int16
+	binary.Read(buf, binary.BigEndian,&v)
+	return v
 }
 
 //ReadNInt32 读取int32
@@ -46,6 +53,11 @@ func (buf *NativeBuffer)WriteNInt16(v int)  {
 	binary.Write(buf, binary.BigEndian, int16(v))
 }
 
+//WriteNShort 写入int16
+func (buf *NativeBuffer)WriteNShort(v int16)  {
+	binary.Write(buf, binary.BigEndian, v)
+}
+
 //WriteNInt32 写入int32
 func (buf *NativeBuffer)WriteNInt32(v int32)  {
 	binary.Write(buf, binary.BigEndian, v)
@@ -54,30 +66,4 @@ func (buf *NativeBuffer)WriteNInt32(v int32)  {
 //WriteNInt64 写入int64
 func (buf *NativeBuffer)WriteNInt64(v int64)  {
 	binary.Write(buf, binary.BigEndian, v)
-}
-
-//NewClientByte 获取前端发送字符
-func NewClientByte(cmd CmdCode)[]byte  {
-	buf := new(NativeBuffer)
-	buf.Buffer = new(bytes.Buffer)
-	var init int32
-	var init2 int64
-	buf.WriteNInt32(init)
-	buf.WriteNInt64(init2)
-	cmd.Write(buf)
-	return buf.Bytes()
-}
-
-//NewClientRetByte 获取后端发送字符
-func NewClientRetByte(data []byte,cmd CmdCode)  {
-	buf := new(NativeBuffer)
-	buf.Buffer = bytes.NewBuffer(data)
-	buf.ReadNInt32()
-	buf.ReadNInt16()
-	buf.ReadNInt32()
-	code := buf.ReadNInt16()
-	if code == 10005{
-
-	}
-	cmd.Read(buf)
 }
